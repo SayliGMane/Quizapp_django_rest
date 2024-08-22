8392980b533801da9929e131444ca43e2622f864
Sayli <saylinavale89@gmail.com>
2024-08-22 14:29:45 +0200

    modified questions model v2

--- apps/questions/models.py
+++ apps/questions/models.py
@@ -1,23 +1,45 @@
 from django.db import models
-from apps.core.constant import LEVEL,TOPIC
+from apps.core.constant import LEVEL, TOPIC
 from django.contrib.postgres.fields import ArrayField
 
-
-# Create your models here.
 class Question(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    question = models.TextField(max_length=2000)  # NOT NULL
+    select_level = models.CharField(max_length=40, choices=LEVEL, default='1')
+    select_topic = models.CharField(max_length=80, choices=TOPIC, default='Basics_1')
+    correct_answer = ArrayField(models.CharField(max_length=1000), null=False)
+    incorrect_answer = ArrayField(models.CharField(max_length=1000), null=False)
+    hint = ArrayField(models.CharField(max_length=1000), null=False)
+    score = models.SmallIntegerField(null=False)
+    retried_quiz = models.BooleanField(default=False)
+
+
+
+    def __str__(self) -> str:
+        return f'{self.question}'
+
+
+# from django.db import models
+# from apps.core.constant import LEVEL,TOPIC
+# from django.contrib.postgres.fields import ArrayField
+
+
+# # Create your models here.
+# class Question(models.Model):
         
-        id = models.CharField(max_length=50, primary_key=True)
-        question= models.TextField (max_length=2000) # need to ask  #NOT NULL
-        select_level = models.CharField(max_length=40, choices=LEVEL, default='1')
-        select_topic = models.CharField (max_length=80, choices=TOPIC, default='Basics_1')
-        correct_answer = ArrayField(models.CharField(max_length=1000), null=False)
-        incorrect_answer = ArrayField(models.CharField(max_length=1000),null=False)
-        hint = ArrayField(models.CharField(max_length=1000), null=False)
-        score = models.SmallIntegerField(null=False)
+#         id = models.CharField(max_length=50, primary_key=True)
+#         question= models.TextField (max_length=2000) # need to ask  #NOT NULL
+#         select_level = models.CharField(max_length=40, choices=LEVEL, default='1')
+#         select_topic = models.CharField (max_length=80, choices=TOPIC, default='Basics_1')
+#         correct_answer = ArrayField(models.CharField(max_length=1000), null=False)
+#         incorrect_answer = ArrayField(models.CharField(max_length=1000),null=False)
+#         hint = ArrayField(models.CharField(max_length=1000), null=False)
+#         score = models.SmallIntegerField(null=False)
+#         date = 
 
 
-        def __str__(self) -> str:
-          return f'{self.question} '
+#         def __str__(self) -> str:
+#           return f'{self.question} '
         
 
 
