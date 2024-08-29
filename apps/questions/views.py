d3e392c9b824f535d4cafb9da447406c970d8312
Sayli <saylinavale89@gmail.com>
2024-08-29 22:43:09 +0200

    delete view added

--- apps/questions/views.py
+++ apps/questions/views.py
@@ -13,25 +13,22 @@ from rest_framework.authentication import BasicAuthentication,TokenAuthenticatio
 def display_questions_by_level(request,level,topic):
     #ORM fecth data from table
     list_questions = Question.objects.filter(select_level=level,select_topic=topic)
-    #Serialiser  pass it to serilaizer
-    
+    #Serialiser pass it to serilaizer
     list_questions=list_questions[0:10]
     data = QuestionSerializer(data=list_questions, many=True)
     data.is_valid()
     #Retun
     return Response(data.data, status=status.HTTP_200_OK)
-   
-    # #ORM
-    # list_questions = list[Question.objects.filter(select_level=level,select_topic=topic)]
-    # #Serialiser
-    # random.shuffle(list_questions) 
-    # list_questions=list_questions[0:10]
-    # data = QuestionSerializer(data=list_questions, many=True)
-    # data.is_valid()
-    # #Retun
-    # return Response(data.data, status=status.HTTP_200_OK)
+# #ORM
+# list_questions = list[Question.objects.filter(select_level=level,select_topic=topic)]
+# #Serialiser
+# random.shuffle(list_questions) 
+# list_questions=list_questions[0:10]
+# data = QuestionSerializer(data=list_questions, many=True)
+# data.is_valid()
+# #Retun
+# return Response(data.data, status=status.HTTP_200_OK)
 #http://127.0.0.1:8000/api/v1/questions/level/01/topic/DTV
-
 
 @api_view(['POST'])
 @authentication_classes([BasicAuthentication])
@@ -41,22 +38,21 @@ def create_question(request):
     serializer = QuestionSerializer(data=data)
 
     if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    serializer.save()
+    return Response(serializer.data,status=status.HTTP_201_CREATED)
     else: 
-        #print("problem")
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
+    #print("problem")
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 #{
-#     "id": "OOP01001",
-#     "question": "What is the core concept of object-oriented programming (OOP)?",
-#     "select_level": "1",
-#     "select_topic": "OOP",
-#     "correct_answer": ["Objects and classes"],
-#     "incorrect_answer": ["Data structures", "Algorithms", "Functions"],
-#     "hint": ["Test"],
-#     "score": 5
+# "id": "OOP01001",
+# "question": "What is the core concept of object-oriented programming (OOP)?",
+# "select_level": "1",
+# "select_topic": "OOP",
+# "correct_answer": ["Objects and classes"],
+# "incorrect_answer": ["Data structures", "Algorithms", "Functions"],
+# "hint": ["Test"],
+# "score": 5
 # }
 
 #242ebe3b906d639539c5d45014bbf7f0509be117
@@ -65,12 +61,10 @@ def create_question(request):
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def delete_question(request,id):
-    
     question= Question.objects.get(id=id)
     if question :
         question.delete()
         return Response(question.id,status=status.HTTP_200_OK) 
-    return Response({"detail": "Question not found."},status=status.HTTP_404_NOT_FOUND)   
+    return Response({"detail": "Question not found."},status=status.HTTP_404_NOT_FOUND) 
 
 
-    
