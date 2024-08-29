e8fd7295acf8720d138c5d4c1be3c68f32e4fda7
Sayli <saylinavale89@gmail.com>
2024-08-29 09:27:20 +0200

    created create question view not tested

--- apps/questions/urls.py
+++ apps/questions/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = 'questions-url'
 
 urlpatterns = [
-    path('level/<level>/topic/<topic>',views.display_questions_by_level,name='list-questions')
+    path('level/<level>/topic/<topic>',views.display_questions_by_level,name='list-questions'),
+    path('create',views.create_question,name='create-questions')
 ]
--- apps/questions/views.py
+++ apps/questions/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from apps.questions.models import Question
 from apps.questions.serializers import QuestionSerializer
 from rest_framework.response import Response
 from rest_framework import status
 import random
+from rest_framework.permissions import IsAuthenticated
 #Create your views here.
 
 @api_view(['GET'])
 def display_questions_by_level(request,level,topic):
    
     #ORM
-    list_questions = Question.objects.filter(select_level=level,select_topic=topic)[:10]
+    list_questions = list[Question.objects.filter(select_level=level,select_topic=topic)]
     #Serialiser
+    random.shuffle(list_questions) 
+    list_questions=list_questions[0:10]
     data = QuestionSerializer(data=list_questions, many=True)
     data.is_valid()
     #Retun
@@ -20,3 +23,27 @@ def display_questions_by_level(request,level,topic):
 
 
 
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def create_question(request):
+#     data = request.data
+#     serializer = QuestionSerializer(data=data)
+
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data,status=status.HTTP_201_CREATED)
+#     else: 
+#         #print("problem")
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+# data = '{
+#     "id": "OOP01001",
+#     "question": "What is the core concept of object-oriented programming (OOP)?",
+#     "select_level": "1",
+#     "select_topic": "OOP",
+#     "correct_answer": ["Objects and classes"],
+#     "incorrect_answer": ["Data structures", "Algorithms", "Functions"],
+#     "hint": ["Test"],
+#     "score": 5
+# }'
